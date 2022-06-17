74a4fb8d301d2995f222e30302311df62c3f271d
Schmostrong <julian.engler14@gmail.com>
2022-06-17 20:22:50 +0200

    Began saving of spawnpoints into database - but - not finished yet

--- src/og/spigot/survival/spawnplugin/database/DAO.java
+++ src/og/spigot/survival/spawnplugin/database/DAO.java
@@ -1,9 +1,25 @@
 package og.spigot.survival.spawnplugin.database;
 
+import java.sql.PreparedStatement;
+
 public class DAO {
     private DatabaseConnection connection;
 
     public DAO(DatabaseConnection connection) {
         this.connection = connection;
+    }
+
+    public void saveGlobalSpawn(){
+        try{
+            this.connection.openConnection();
+
+            PreparedStatement ps = this.connection.getConnection().prepareStatement("INSERT INTO OGSpawnPoints VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        }catch (Exception ex){
+
+        }finally {
+            if(connection.getConnection() != null){
+                connection.closeConnection();
+            }
+        }
     }
 }
